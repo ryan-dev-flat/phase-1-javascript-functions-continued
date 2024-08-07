b626f7d8eb6109a61cf4c158781ab9a873aa85ea
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-07 14:53:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`${activity}`)
+
+return `This Saturday, I want to ${activity}!`
+};
+
+
+function mondayWork(workLocation = 'go to the office') {
+    console.log(`${workLocation}`)
+    return `This Monday, I will ${workLocation}.`
+};
+
+
+function wrapAdjective(flair = '*') {
+    return function adjective(word = 'special') {
+
+        return `You are ${flair}${word}${flair}!`;
+    }
+}
+console.log(wrapAdjective());
